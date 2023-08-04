f4be4e9226a67b926071b3d68d5a085de36e5111
Kartargit <skartar61962@gmail.com>
2023-08-04 22:06:43 +0530

    gearChanged

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -19,24 +19,27 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-        newSpeed += rate;
-        if(newSpeed<=50)changeGear(1);
-        else if (newSpeed<=100)changeGear(2);
-        else if (newSpeed<=150)changeGear(3);
-        else if (newSpeed<=200)changeGear(4);
-        else if (newSpeed<=250)changeGear(5);
-        else if (newSpeed>250) {
-            changeGear(6);
-        }
+        newSpeed = getCurrentSpeed() + rate;
+
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             super.stop();
+            super.changeGear(1);
         }
+
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
-
             changeSpeed(newSpeed, getCurrentDirection());
+
+            if(newSpeed<=50)super.changeGear(1);
+            else if (newSpeed<=100)super.changeGear(2);
+            else if (newSpeed<=150)super.changeGear(3);
+            else if (newSpeed<=200)super.changeGear(4);
+            else if (newSpeed<=250)super.changeGear(5);
+            else if (newSpeed>250) {
+                super.changeGear(6);
+            }
         }
     }
 }
